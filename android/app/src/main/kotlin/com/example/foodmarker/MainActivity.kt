01fe316c1f5f09b64163f6e977292fcfd3bfebca
nocwalker7 <nocwalker7@gmail.com>
2021-07-04 10:37:57 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/foodmarker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foodmarker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:foodmarker/shared/shared.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'general_page.dart';
+part 'sign_in_page.dart';
+part 'sign_up_page.dart';
