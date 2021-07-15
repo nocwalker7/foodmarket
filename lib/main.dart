6008b0e4ae8109ff48ae4c327d97d2f0641619a2
nocwalker7 <nocwalker7@gmail.com>
2021-07-15 22:55:20 +0700

    resume build signup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'ui/pages/pages.dart';
 
@@ -9,9 +10,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUpPage(),
+      home: SignInPage(),
     );
   }
 }
--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -15,8 +15,51 @@ class _SignUpPageState extends State<SignUpPage> {
     return GeneralPage(
         title: 'Sign Up',
         subtitle: "Register and Eat",
+        onBackButtonPressed: () {
+          Get.back();
+        },
         child: Column(
           children: [
+            Container(
+              width: 110,
+              height: 110,
+              margin: EdgeInsets.only(top: 26),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/photo_border.png'))),
+              child: Container(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://cdn.iconscout.com/icon/free/png-256/user-avatar-contact-portfolio-personal-portrait-profile-2-5270.png'),
+                        fit: BoxFit.cover)),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.fromLTRB(defaultMargin, 16, defaultMargin, 6),
+              child: Text(
+                "Full Name",
+                style: blackFontStyle2,
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.black)),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintStyle: greyFontStyle,
+                    hintText: 'Type your full name'),
+              ),
+            ),
             Container(
               width: double.infinity,
               margin: EdgeInsets.fromLTRB(defaultMargin, 26, defaultMargin, 6),
