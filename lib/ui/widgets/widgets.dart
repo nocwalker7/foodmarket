e642953e08f1f4c0eacc291670a5d9733ae01aa0
nocwalker7 <nocwalker7@gmail.com>
2021-07-31 11:06:32 +0700

    Making Custom Tab Bar on Food Page

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:foodmarket/shared/shared.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:foodmarket/models/models.dart';
+import 'package:supercharged/supercharged.dart';
+import 'package:get/get.dart';
 
 part 'custom_bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
+part 'custom_tabbar.dart';
